adb2b8ad1d930138ec7e26687ff83bc03ea2a763
aleecalderon <alejandracristal162@gmail.com>
2025-09-24 13:31:06 -0600

    Cambios migracion

---
+++ Migrations/20250924184212_NombreNuevaMigracion.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventarioProductos.Migrations
+{
+    /// <inheritdoc />
+    public partial class NombreNuevaMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
+
+//nueva migracion agregada 
